63f107b42b38ebf987585f25246dce671a22d3f5
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-30 17:59:50 +0600

    add file add

---
+++ add.js
@@ -0,0 +1,9 @@
+function add(num1, num2){
+ var sum = num1 +num2;
+ return sum;
+}
+var result1 = add(12, 13);
+var result2 = add (37, 10);
+const finalResult = add(result1 , result2);
+// const total = finalResult;
+console.log(finalResult);
