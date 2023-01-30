847dc789f868d8eb62f88ae5bd362458053a3a3a
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-30 17:17:20 +0600

    function file add and return

---
+++ function.js
@@ -0,0 +1,9 @@
+function craditCard () {
+    var total = shirt + pant;
+    return total;
+}
+var shirt =120;
+
+var pant = 180;
+var finalResult = craditCard(shirt, pant);
+console.log(finalResult);
