93c04e2519d1982e783ff950d91fb07c74e01f1e
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-30 19:13:30 +0600

    object file add

---
+++ object.js
@@ -0,0 +1,11 @@
+const myComputer = {
+    brand : 'lonovo',
+    price : 39000,
+    color : 'silver',
+    processor :'i7'
+
+}
+myComputer.brand = 'linux',
+//  computer = myComputer.
+console.log(myComputer.processor);
+// console.log(myComputer);
